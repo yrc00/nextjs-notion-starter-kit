386b4a676c06960460d2d0913b9c02626ada50a7
yrc00 <mailharu365@gmail.com>
2025-02-07 15:59:11 +0900

    Edit blog information

--- site.config.ts
+++ site.config.ts
@@ -2,31 +2,31 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '19240ad369648021994deab8af3308e5',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Yerim-DevNote',
+  domain: 'yerim-devnote.vercel.app',
+  author: 'Yerim',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Yerim DevNote - The place where I share my knowledge and experience',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  // twitter: 'transitive_bs',
+  github: 'yrc00',
+  linkedin: '예림-최-4623882b5',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
+  defaultPageIcon: 'https://www.notion.so/image/attachment%3A15aec9b7-d613-49bd-9f02-c4102bba1bd8%3Aavatar_small.png?table=block&id=19240ad3-6964-8021-994d-eab8af3308e5&spaceId=74d72aed-20c7-4845-8ef9-3da7107fec1e&width=250&userId=696885ec-a140-4fc8-8aee-69a463094eae&cache=v2',
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
@@ -50,16 +50,20 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: '19240ad3696480998785f0b1ffd527a7'
+    },
+    {
+      title: 'CV',
+      pageId: '19240ad3696480d8bd4ef4017284eb9e'
+    },
+    {
+      title: 'Category',
+      pageId: '19240ad369648002a8dbd058c0960c3d'
+    }
+  ]
 })
